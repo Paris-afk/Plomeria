30fbb3779a0f97a7362756f300e2c3caa3615691
Paris López <paris.lopez@isita.com.mx>
2021-12-05 12:29:39 -0600

    map working

--- app/src/main/java/com/example/plmeria/ubicacionMapa.kt
+++ app/src/main/java/com/example/plmeria/ubicacionMapa.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.huawei.hms.maps.HuaweiMap
+import com.huawei.hms.maps.MapsInitializer
 import com.huawei.hms.maps.OnMapReadyCallback
 import com.huawei.hms.maps.SupportMapFragment
 
 class ubicacionMapa : AppCompatActivity(), OnMapReadyCallback {
     val TAG = "MyMessage"
+    private var hMap: HuaweiMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        MapsInitializer.setApiKey("CwEAAAAAV/RWwFd4r1PxAhZqXNtVUYAEuX3/VnrtumypLqyj73FHRTCW0UxiKLT+cTWIK5B1SlJBo87UbC0mvnzXmMWlnePYsIY=")
+        setContentView(R.layout.activity_ubicacion_mapa)
 
         var mSupportMapFragment: SupportMapFragment? = null
         mSupportMapFragment = supportFragmentManager.findFragmentById(R.id.mapfragment_mapfragmentdemo) as SupportMapFragment?
@@ -19,7 +24,8 @@ class ubicacionMapa : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(huaweiMap: HuaweiMap) {
         Log.d(TAG, "onMapReady: ")
-        var hMap = huaweiMap
+         hMap = huaweiMap
 
     }
+
 }
